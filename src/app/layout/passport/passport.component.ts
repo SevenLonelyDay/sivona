4b2c79ad58cea624029d3c5338c97213645d4052
张珂琦 <admin@zhangkeqideiMac.local>
2019-07-18 17:59:30 +0800

    添加注解以及更新粒子图

--- src/app/layout/passport/passport.component.ts
+++ src/app/layout/passport/passport.component.ts
@@ -128,35 +128,152 @@ export class LayoutPassportComponent implements OnInit {
     width: number = 100;
     height: number = 100;
     myStyle: object = {
-        'position': 'fixed',
-        'width': '100%',
-        'height': '100%',
+        position: 'fixed',
+        width: '100%',
+        height: '100%',
         'z-index': -1,
-        'top': 0,
-        'left': 0,
-        'right': 0,
-        'bottom': 0,
+        background: '#474747',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
     };
     myParams: object = {
         particles: {
             number: {
-                value: 200,
+                value: 80,
+                density: {
+                    enable: true,
+                    value_area: 800,
+                },
             },
             color: {
-                value: '#000'
+                value: '#ffffff',
             },
             shape: {
                 type: 'circle',
-                // image: {
-                //     src:'https://gw.alipayobjects.com/zos/rmsportal/TVYTbAXWheQpRcWDaDMu.svg'
-                //     src: '/assets/imgs/black147.jpg'
-                // }
+                stroke: {
+                    width: 0,
+                    color: '#000000',
+                },
+                polygon: {
+                    nb_sides: 5,
+                },
+                image: {
+                    src: 'img/github.svg',
+                    width: 100,
+                    height: 100,
+                },
             },
-        }
+            opacity: {
+                value: 0.5,
+                random: false,
+                anim: {
+                    enable: false,
+                    speed: 1,
+                    opacity_min: 0.1,
+                    sync: false,
+                },
+            },
+            size: {
+                value: 5,
+                random: true,
+                anim: {
+                    enable: false,
+                    speed: 40,
+                    size_min: 0.1,
+                    sync: false,
+                },
+            },
+            line_linked: {
+                enable: true,
+                distance: 150,
+                color: '#ffffff',
+                opacity: 0.4,
+                width: 1,
+            },
+            move: {
+                enable: true,
+                speed: 6,
+                direction: 'none',
+                random: false,
+                straight: false,
+                out_mode: 'out',
+                attract: {
+                    enable: false,
+                    rotateX: 600,
+                    rotateY: 1200,
+                },
+            },
+        },
+        interactivity: {
+            detect_on: 'canvas',
+            events: {
+                onhover: {
+                    enable: true,
+                    mode: 'repulse',
+                },
+                onclick: {
+                    enable: true,
+                    mode: 'push',
+                },
+                resize: true,
+            },
+            modes: {
+                grab: {
+                    distance: 400,
+                    line_linked: {
+                        opacity: 1,
+                    },
+                },
+                bubble: {
+                    distance: 400,
+                    size: 40,
+                    duration: 2,
+                    opacity: 8,
+                    speed: 3,
+                },
+                repulse: {
+                    distance: 200,
+                },
+                push: {
+                    particles_nb: 4,
+                },
+                remove: {
+                    particles_nb: 2,
+                },
+            },
+        },
+        retina_detect: true,
+        config_demo: {
+            hide_card: false,
+            background_color: '#b61924',
+            background_image: '',
+            background_position: '50% 50%',
+            background_repeat: 'no-repeat',
+            background_size: 'cover',
+        },
     };
-    constructor(@Inject(WINDOW) private win: Window, private router: Router) { }
+    // myParams: object = {
+    //     particles: {
+    //         number: {
+    //             value: 200,
+    //         },
+    //         color: {
+    //             value: '#c7d5e0',
+    //         },
+    //         shape: {
+    //             type: 'circle',
+    //             // image: {
+    //             //     src:'https://gw.alipayobjects.com/zos/rmsportal/TVYTbAXWheQpRcWDaDMu.svg'
+    //             //     src: '/assets/imgs/black147.jpg'
+    //             // }
+    //         },
+    //     },
+    // };
+    constructor(@Inject(WINDOW) private win: Window, private router: Router) {}
 
-    ngOnInit() { }
+    ngOnInit() {}
 
     to(item: GlobalFooterLink) {
         if (!item.href) {
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -13,6 +13,56 @@ import { registerLocaleData } from '@angular/common';
 import zh from '@angular/common/locales/zh';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 registerLocaleData(zh);
+import { BehaviorSubject } from 'rxjs';
+
+export function ObservableInput<
+    T = any,
+    SK extends keyof T = any,
+    K extends keyof T = any
+>(propertyKey?: K | boolean, initialValue?: any) {
+    return (target: T, sPropertyKey: SK) => {
+        const symbol = Symbol();
+
+        type ST = any;
+
+        type Mixed = T & {
+            [symbol]: BehaviorSubject<ST>;
+        } & Record<SK, BehaviorSubject<ST>>;
+
+        Object.defineProperty(target, sPropertyKey, {
+            enumerable: true,
+            configurable: true,
+            get(this: Mixed) {
+                return (
+                    this[symbol] ||
+                    (this[symbol] = new BehaviorSubject<ST>(initialValue))
+                );
+            },
+            set(this: Mixed, value: ST) {
+                this[sPropertyKey].next(value);
+            },
+        });
+
+        if (!propertyKey) {
+            return;
+        }
+
+        if (propertyKey === true) {
+            propertyKey = (sPropertyKey as string).replace(/\$+$/, '') as K;
+        }
+
+        Object.defineProperty(target, propertyKey, {
+            enumerable: true,
+            configurable: true,
+            get(this: Mixed) {
+                return this[sPropertyKey].getValue();
+            },
+            set(this: Mixed, value: ST) {
+                this[sPropertyKey].next(value);
+            },
+        });
+    };
+}
 
 @NgModule({
     declarations: [],
